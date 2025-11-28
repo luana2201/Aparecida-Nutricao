f89271fbe5530b5d95dc7848e7316f349a538456
luana2201 <souza.siqueira.luana@escola.pr.gov.br>
2025-11-28 08:37:13 -0300

    buscar-pacientes

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,7 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function(){
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokaupp.com/pacientes");
+});
